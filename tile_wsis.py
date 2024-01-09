c61b6985a4d2de1e00295055e18632a1fc497994
Sbrussee <Siemenbrussee@hotmail.com>
2024-01-09 15:52:29 +0100

    test

--- tile_wsis.py
+++ tile_wsis.py
@@ -1,3 +1,35 @@
+import slideflow as sf
+
+
+def tile_wsis(dataset):
+    dataset.extract_tiles(
+    qc='both', #Both use Otsu Thresholding and Blur detection
+    normalizer="macenko",
+    whitespace_fraction="0.75"
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    project = sf.create_project(
+    root="./mf/"
+    annotations = "../../train_list_definitive.csv",
+    slides = "../../MF_AI_dataset_cropped"
+    )
+
+    dataset = project.dataset(tile_px=512, tile_um="40x")
+    print(dataset.summary())
+
+    tile_wsis(dataset)
+
 if __name__ == "__main__":
     main()
-
