b34a15b937f6cbc68b760c198959e72f32f6061b
Sbrussee <Siemenbrussee@hotmail.com>
2024-01-09 16:50:08 +0100

    now processes annotation file

--- tile_wsis.py
+++ tile_wsis.py
@@ -1,5 +1,11 @@
 import slideflow as sf
+import pandas as pd
+import os
 
+def process_annotation_file(original_path):
+    df = pd.read_csv(original_path)
+    df.rename(columns={'case_id' : 'patient', 'slide_id' : slide}, inplace=True)
+    df.to_csv(f"{os.path.basename(original_path)}_slideflow.csv", index=False)
 
 def tile_wsis(dataset):
     dataset.extract_tiles(
@@ -12,6 +18,7 @@ def tile_wsis(dataset):
 
 
 def main():
+
     project = sf.create_project(
     root = "./mf/",
     annotations = "../../train_list_definitive.csv",
@@ -24,4 +31,7 @@ def main():
     tile_wsis(dataset)
 
 if __name__ == "__main__":
+    annotations = "../../train_list_definitive.csv"
+    if not os.path.exists(f"{os.path.basename(original_path)}_slideflow.csv"):
+        process_annotation_file(annotations)
     main()
