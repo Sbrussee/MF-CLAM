c185bd42798942fd62351100877716a0c6741db2
Sbrussee <Siemenbrussee@hotmail.com>
2024-03-08 14:08:01 +0100

    test

--- mf_script.py
+++ mf_script.py
@@ -131,7 +131,9 @@ def split_dataset(dataset, test_fraction=0.2):
     train, test = dataset.split(
     model_type="categorical",
     val_strategy='fixed',
-    val_fraction=test_fraction
+    labels="category"
+    val_fraction=test_fraction,
+    outcome_key='category'
     )
 
     train = train.balance(headers='label', strategy=args.training_balance)
