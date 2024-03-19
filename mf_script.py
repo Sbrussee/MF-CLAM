c3bdc9313e8cba317576168fbfe179a14179a64f
Sbrussee <Siemenbrussee@hotmail.com>
2024-03-19 16:29:11 +0100

    test

--- mf_script.py
+++ mf_script.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 os.environ['NEPTUNE_API_TOKEN'] = "eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI3MWI4Zjg5My04NTdlLTQxMGItYTcwZC02ZGIwMmE0NWUwMjQifQ=="
-os.environ['NEPTUNE_WORKSPACE'] = "siemenbrussee/MF-CLAM"
+os.environ['NEPTUNE_WORKSPACE'] = "siemenbrussee"
 
 parser = argparse.ArgumentParser()
 #Global arguments
