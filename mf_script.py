968c1564c5911e677233ab52f58a8c0ae7470223
Sbrussee <Siemenbrussee@hotmail.com>
2024-03-29 13:58:40 +0100

    test

--- mf_script.py
+++ mf_script.py
@@ -155,7 +155,7 @@ class barlow_twins_feature_extractor(TorchFeatureExtractor):
 
         self.transform = transforms.Compose([
             transforms.Resize(tile_px),
-            #transforms.Lambda(lambda x: x / 255.),
+            transforms.Lambda(lambda x: x / 255.),
             transforms.Normalize(
                 mean=[0.485, 0.456, 0.406],
                 std=[0.229, 0.224, 0.225]
